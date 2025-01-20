676618eeca56a32130cf9516ed1b803cb18b014f
Nate <48430676+nateurl@users.noreply.github.com>
2025-01-20 22:15:37 +1300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     particlesJS('particles-js', {
         particles: {
             number: { value: 80, density: { enable: true, value_area: 800 } },
-            color: { value: "#00ffff" },
+            color: { value: "#ffffff" },
             shape: { type: "circle" },
             opacity: { value: 0.5, random: true },
             size: { value: 3, random: true },
             line_linked: {
                 enable: true,
                 distance: 150,
-                color: "#00ffff",
+                color: "#ffffff",
                 opacity: 0.4,
                 width: 1
             },
